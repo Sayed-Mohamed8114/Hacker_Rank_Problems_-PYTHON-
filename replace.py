ab79fd56f99a235baf8511e8044b1b555854e965
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-08-19 21:31:31 +0300

    replace the white spaces with -

---
+++ replace.py
@@ -0,0 +1,9 @@
+# replace the white blanks with "-"
+
+def split_and_join(line):
+    return line.replace(" ", "-")
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
