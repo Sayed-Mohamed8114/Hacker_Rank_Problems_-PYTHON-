7c4287d9ac76853f42c4742cc6972c493fc100e5
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-08-21 17:51:54 +0300

    the basics operations

---
+++ simpleCAlculator.py
@@ -0,0 +1,15 @@
+
+num1=int(input('enter first number: ').strip())
+num2=int(input('enter second number: ').strip())
+
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+result1=num1//num2
+result2=num1/num2
+
+print(sum)
+print(difference)
+print(product)
+print(result1)
+print(result2)
