ebec9207892d79fafc8d1718256e65421ecfb5b4
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-08-31 18:44:18 +0300

    now we have two ways to return the second greatest

--- secondscore.py
+++ secondscore.py
@@ -6,3 +6,22 @@ if len(unique_numbers )>1:
     print(f'second element is {unique_numbers[1]}')
 else:
     print('no second largest number ') 
+    
+#the second way very important*******
+'''
+n=int(input('enter the len of the list: ').strip())
+
+while True:
+     numbers = list(map(int, input("Enter numbers separated by spaces: ").strip().split()))
+     if len(numbers)==n:
+         break
+     else:
+         continue
+
+unique_numbers= sorted(set(numbers), reverse=True)
+
+if len(unique_numbers )>1:
+    print(f'second element is {unique_numbers[1]}')
+else:
+    print('no second largest number ') 
+'''
