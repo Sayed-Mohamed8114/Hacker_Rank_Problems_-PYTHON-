2b68c6bf757844b4111e8742b385d25d7f912fca
Sayed-Mohamed8114 <sayed.mohamed8114@gmail.com>
2025-09-02 01:37:48 +0300

    calculate the time of the operation

---
+++ time.py
@@ -0,0 +1,9 @@
+from time import time
+start_time=time()
+x=6
+end_time=time()
+elapsed=end_time-start_time
+print(elapsed)
+
+#a code to calculate the processing time 
+#it can be affected by the other running items on the pc ,because the (cpu)
